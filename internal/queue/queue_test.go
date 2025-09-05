7b4ea6730e3b517b9ceefe1365bab97273c74d4b
Tharun Ragulan <t2ragula@uwaterloo.ca>
2025-09-05 15:18:53 -0400

    refactored test to run for email and api

--- internal/queue/queue_test.go
+++ internal/queue/queue_test.go
@@ -1,72 +1,62 @@
 package queue
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/tharun-ragu22/job_scheduler/internal/jobs"
 )
 
+type JobTestCase struct {
+    name string
+    job jobs.Job
+    expectedType reflect.Type
 
+}
 
-func Test_Enqueue_WhenEmailJobAddedToScheduler_JobAddedToScheduler(t *testing.T) {
-    // Given a user wants to send an email
-    scheduler := &Scheduler{}
+func Test_Enqueue_WhenJobAddedToScheduler_JobAddedToScheduler(t *testing.T) {
+    // Given a user wants to create a task
 
-    // When they submit a request to send an email
     emailJob := &jobs.EmailJob{
         To:      "user@example.com",
         Subject: "Test Email",
         Body:    "Hello World",
     }
 
-    scheduler.Enqueue(emailJob)
-
-	addedJob := scheduler.PeekTop()
-
-    // Then the system accepts the task and schedules it for processing
-    if addedJob == nil {
-		t.Fatalf("Expected a job to be added to the scheduler, got nil")
-	}
-
-    queuedJob, ok := (*addedJob).(*jobs.EmailJob)
-    if !ok {
-        t.Fatalf("Expected job to be of type *EmailJob")
-    }
-
-    if queuedJob.To != "user@example.com" || queuedJob.Subject != "Test Email" {
-        t.Errorf("Queued job does not match submitted job")
-    }
-}
-
-
-func Test_Enqueue_WhenAPIRequestAddedToScheduler_JobAddedToScheduler(t *testing.T) {
-    // Given a user wants to make an API request
-    scheduler := &Scheduler{}
-
-    example_url := "www.example.com"
-    example_method := "POST"
-
-    // When they submit a request to send the request
     apiJob := &jobs.APIJob{
-        URL:      example_url,
-        Method: example_method,
+        URL:      "www.example.com",
+        Method: "POST",
     }
 
-    scheduler.Enqueue(apiJob)
-
-	addedJob := scheduler.PeekTop()
-
-    // Then the system accepts the request and schedules it for processing
-    if addedJob == nil {
-		t.Fatalf("Expected a job to be added to the scheduler, got nil")
-	}
-
-    queuedJob, ok := (*addedJob).(*jobs.APIJob)
-    if !ok {
-        t.Fatalf("Expected job to be of type *APIJob")
+    testCases := []JobTestCase{
+        {
+            name: "Email Job",
+            job:          emailJob,
+            expectedType: reflect.TypeOf(&jobs.EmailJob{}),
+        },
+        {
+            name: "API Job",
+            job:          apiJob,
+            expectedType: reflect.TypeOf(&jobs.APIJob{}),
+        },
     }
 
-    if queuedJob.URL != example_url || queuedJob.Method != example_method {
-        t.Errorf("Queued job does not match submitted job")
+    for _, tc := range testCases {
+    
+        scheduler := &Scheduler{}
+
+        // When they submit the required information for the task
+        scheduler.Enqueue(tc.job)
+
+        // Then the system accepts the task and schedules it for processing
+        addedJob := scheduler.PeekTop()
+
+        if addedJob == nil {
+            t.Fatalf("Expected a job to be added to the scheduler, got nil")
+        }
+        
+        if *addedJob != tc.job {
+            t.Errorf("Queued job does not match submitted job %s", tc.name)
+        }
     }
 }
